f3c470ea32173d0faf22c1881d83170ae5af17e8
mede-mas <mede-mas@student.42.fr>
2024-05-02 16:00:29 +0200

    ServerManager: adding CGI handling

--- src/Server/ServerManager.cpp
+++ src/Server/ServerManager.cpp
@@ -6,7 +6,7 @@
 /*   By: mede-mas <mede-mas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 12:15:01 by  rokupin          #+#    #+#             */
-/*   Updated: 2024/04/08 15:51:15 by mede-mas         ###   ########.fr       */
+/*   Updated: 2024/05/02 15:50:15 by mede-mas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,44 @@ void ServerManager::Stop(int signal) {
 	if (signal == SIGINT || signal == SIGSTOP) {
 		std::cout << "\nStopping servers..." << std::endl;
 		is_running_ = false;
+	}
+}
+
+// Helper function to extract the extension from a URL
+std::string ServerManager::GetExtensionFromURL(const std::string &url) {
+	size_t dot_pos = url.rfind('.');
+	if (dot_pos == std::string::npos)
+		return "";		// No extension found
+	return url.substr(dot_pos);
+}
+
+// Executes the CGI script and processes its output
+void ServerManager::ExecuteCGIScript(Connection &connection, const std::string &cgi_path) {
+	// Prepare environment variables
+	std::vector<std::string> env_strings; // Store strings for environment
+	std::vector<char*> env; // Store pointers for execve
+	std::string method = "REQUEST_METHOD=" + connection.getMethod();
+	std::string query_string = "QUERY_STRING=" + connection.getQueryString();
+	std::string content_type = "CONTENT_TYPE=" + connection.getContentType();
+	env_strings.push_back(method);
+	env_strings.push_back(query_string);
+	env_strings.push_back(content_type);
+
+	for (size_t i = 0; i < env_strings.size(); ++i)
+		env.push_back(const_cast<char*>(env_strings[i].c_str()));
+	env.push_back(NULL);
+
+	// Create the CGI process (fork, execve)
+	pid_t pid = fork();
+	if (pid == 0) { // Child process
+		// Execute the CGI script
+		char *args[] = { const_cast<char*>(cgi_path.c_str()), NULL };
+		execve(cgi_path.c_str(), args, env.data());
+		_exit(1); // Exit on failure
+	} else if (pid > 0) { // Parent process
+		// Handle reading CGI output and writing response to the client
+	} else {
+		// Fork failed, log and handle error
+		Log("Failed to fork the CGI process");
 	}
 }
--- src/Server/ServerManager.h
+++ src/Server/ServerManager.h
@@ -6,7 +6,7 @@
 /*   By: mede-mas <mede-mas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 12:15:01 by  rokupin          #+#    #+#             */
-/*   Updated: 2024/03/18 19:57:03 by mede-mas         ###   ########.fr       */
+/*   Updated: 2024/05/02 15:55:15 by mede-mas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,54 +27,57 @@ class ServerManager {
 public:
 class ServerManagerException : public std::exception {};
 
-    ServerManager();
+	ServerManager();
 
-    ~ServerManager();
+	~ServerManager();
 //-------------------init-------------------------------------------------------
-    void            Init(const Config &config);
-    void            CreateEpollInstance();
-    void            CreateListeningSocket(const ServerConfiguration &conf);
-    void            PresetAddress(addrinfo **addr, const std::string &host,
-                                  const std::string &port_str);
-    int             CreateSocket(addrinfo *res, const std::string &host,
-                                 const std::string &port_str);
-    void            SetSocketOptions(addrinfo *res, int socket);
-    void            BindSocket(addrinfo *res, int socket);
-    void            ListenSocket(int socket);
-    void            AddSocketToEpollInstance(int socket);
+	void            Init(const Config &config);
+	void            CreateEpollInstance();
+	void            CreateListeningSocket(const ServerConfiguration &conf);
+	void            PresetAddress(addrinfo **addr, const std::string &host,
+								  const std::string &port_str);
+	int             CreateSocket(addrinfo *res, const std::string &host,
+								 const std::string &port_str);
+	void            SetSocketOptions(addrinfo *res, int socket);
+	void            BindSocket(addrinfo *res, int socket);
+	void            ListenSocket(int socket);
+	void            AddSocketToEpollInstance(int socket);
 //-------------------run--------------------------------------------------------
-    void            Start();
-    void            EventLoop();
-    bool            AddClientToEpoll(int client_sock);
-    static void     Stop(int signal);
+	void            Start();
+	void            EventLoop();
+	bool            AddClientToEpoll(int client_sock);
+	static void     Stop(int signal);
+	// Declaring function for executing CGI scripts
+	void			ExecuteCGIScript(Connection &connection, const std::string &cgi_path);
 //-------------------handle-----------------------------------------------------
-    const Server    &FindServer(const Connection &connection) const;
-    void            AcceptNewConnection(int server_socket);
-    void            HandleEventsOnExistingConnection(int client_socket);
-    bool            ProcessHeaders(Connection &connection);
-    void            CloseConnectionWithLogMessage(int socket,
-                                                  const std::string &msg);
-    bool            ProcessBody(Connection &connection);
-    bool            Respond(Connection &connection);
-    void            CloseTimedOutConnections();
+	const Server    &FindServer(const Connection &connection) const;
+	void            AcceptNewConnection(int server_socket);
+	void            HandleEventsOnExistingConnection(int client_socket);
+	bool            ProcessHeaders(Connection &connection);
+	void            CloseConnectionWithLogMessage(int socket,
+												  const std::string &msg);
+	bool            ProcessBody(Connection &connection);
+	bool            Respond(Connection &connection);
+	void            CloseTimedOutConnections();
+	// Declaring the helper function to extract the extension from a URL
+	std::string		GetExtensionFromURL(const std::string &url);
 //-------------------util-------------------------------------------------------
-    void            Cleanup();
-    void            PrintEventInfo(int events, int fd, int i) ;
-    void            ThrowException(const std::string &msg) const;
-    void            Log(const std::string &msg) const;
-    bool            IsListeningSocketFd(int socket) const;
-    bool            SetDescriptorNonBlocking(int sockfd) const;
+	void            Cleanup();
+	void            PrintEventInfo(int events, int fd, int i) ;
+	void            ThrowException(const std::string &msg) const;
+	void            Log(const std::string &msg) const;
+	bool            IsListeningSocketFd(int socket) const;
+	bool            SetDescriptorNonBlocking(int sockfd) const;
 private:
-    int             epoll_fd_;
-    v_servers       servers_;
-    m_host_int      host_to_socket_; /* quick find-by-host required by CreateListeningSockets */
+	int             epoll_fd_;
+	v_servers       servers_;
+	m_host_int      host_to_socket_; /* quick find-by-host required by CreateListeningSockets */
 
-    v_conn          connections_;
-    int             epoll_returns_count_;
-    int             epoll_events_count_;
-    int             epoll_connection_count_;
-    int             epoll_in_out_count_;
+	v_conn          connections_;
+	int             epoll_returns_count_;
+	int             epoll_events_count_;
+	int             epoll_connection_count_;
+	int             epoll_in_out_count_;
 };
-
 
 #endif //WEBSERV_SERVERMANAGER_H
