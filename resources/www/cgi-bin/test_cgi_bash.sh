334505793587704116420cc5847c5505f34e71ee
mede-mas <mede-mas@student.42.fr>
2024-05-20 15:23:37 +0200

    CGI_script: commenting part requesting data

--- resources/www/cgi-bin/test_cgi_bash.sh
+++ resources/www/cgi-bin/test_cgi_bash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-data=""
-while IFS= read -r line; do
-    data="$data$line\n"
-done
-data_size=$(echo -e "$data" | wc -c)
+# data=""
+# while IFS= read -r line; do
+#     data="$data$line\n"
+# done
+# data_size=$(echo -e "$data" | wc -c)
 #data_size=0
 
 echo "HTTP/1.1 200 OK"
